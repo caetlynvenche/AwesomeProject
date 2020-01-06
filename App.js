ce9479b2dfeaca939d5cc69cd774f5a28223707a
Caetlyn Venche <caetlynvenche@gmail.com>
2020-01-06 16:41:39 -0700

    initial setup for react native project

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Caetlyn Says Hi!!!</Text>
+      <Text>I am working on React Native... finally--</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
