ba802521186fbbd087375042410cf4989b2a8e88
Caetlyn Venche <caetlynvenche@gmail.com>
2020-01-20 14:20:30 -0700

    global styles setup and basic app setup

--- reviewApp/App.js
+++ reviewApp/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+// import { StyleSheet, View } from 'react-native'
+import * as Font from 'expo-font'
+import { AppLoading } from 'expo'
+import HomeScreen from './screens/HomeScreen'
+
+const getFonts = () => Font.loadAsync({
+    'tangerine-regular': require('./assets/fonts/Tangerine-Regular.ttf'),
+    'tangerine-bold': require('./assets/fonts/Tangerine-Bold.ttf')
+})
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+    const [fontsLoaded, setFontsLoaded] = useState(false)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    if(fontsLoaded) {
+        return (
+            <HomeScreen />
+        )
+    } else {
+        return (
+            <AppLoading 
+            startAsync={getFonts}
+            onFinish={() => setFontsLoaded(true)}
+            />
+        )
+    }
+}
